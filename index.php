009bf3c740edc1e1e438ca7f1e1fe214ca31b026
Nichapa <Nichapa.it@gmail.com>
2017-04-23 17:19:59 +0700

    index.php

--- index.php
+++ index.php
@@ -17,9 +17,15 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
+
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => "I don't understand"
+			];
+
+			$messages = [
+				'type' => 'text',
+				'text' => "Hello"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
