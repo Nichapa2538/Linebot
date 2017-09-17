c332a243d26b5b3e79ea84a0ee5ea398133dedfc
Nichapa2538 <nichapa.it@gmail.com>
2017-09-17 18:50:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 // Validate parsed JSON data
 $ph = file_get_contents('https://api.thingspeak.com/channels/321156/fields/1/last.txt');
-//convert
+//convert 
 $Natural = 5 ;
 $Acidic = 6 ;
 $Alkaline = 10 ;	
