8788f24350487185eff7da16aca78cb9f67da376
Nichapa2538 <nichapa.it@gmail.com>
2017-09-22 02:18:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,10 +12,31 @@ $date = date("h:i:sa");
 //convert 
 $Natural = 5 ;
 //$Acidic = 6 ;
-//$Alkaline = 10 ;	
+//$Alkaline = 10 ;
+$str = “Hello”; //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
+ 
+$res = notify_message($str,$token);
+print_r($res);
+function notify_message($message,$token){
+ $queryData = array(‘message’ => $message);
+ $queryData = http_build_query($queryData,’’,’&’);
+ $headerOptions = array( 
+         ‘http’=>array(
+            ‘method’=>’POST’,
+            ‘header’=> “Content-Type: application/x-www-form-urlencoded\r\n”
+                      .”Authorization: Bearer “.$token.”\r\n”
+                      .”Content-Length: “.strlen($queryData).”\r\n”,
+            ‘content’ => $queryData
+         ),
+ );
+ $context = stream_context_create($headerOptions);
+ $result = file_get_contents(LINE_API,FALSE,$context);
+ $res = json_decode($result);
+ return $res;
+}
 
 
-if (!is_null($events['events'])) {
+/*if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
