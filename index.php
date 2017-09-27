1c7459c5de23ea0a0821489fa6d7ab527dba7d12
Nichapa2538 <nichapa.it@gmail.com>
2017-09-27 19:19:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,65 +6,62 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 // Validate parsed JSON data
 $ph = file_get_contents('https://api.thingspeak.com/channels/321156/fields/2/last.txt');
-$vol = file_get_contents('https://api.thingspeak.com/channels/321156/fields/1/last.txt');
+//$ph = file_get_contents('https://api.thingspeak.com/cha.../321156/created_at/last.txt');
 date_default_timezone_set("Asia/Bangkok");
 $date = date("h:i:sa");
 //convert 
 $Natural = 5 ;
 //$Acidic = 6 ;
-//$Alkaline = 10 ;
+//$Alkaline = 10 ;	
+if (!is_null($events['events'])) {
+// Loop through each event
+foreach ($events['events'] as $event) {
+// Reply only when message sent is in 'text' format
+if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+// Get text sent
+$text = $event['message']['text'];
+// Get replyToken
+$replyToken = $event['replyToken'];
 
+// Build message to reply back
+$messages = [
+'type' => 'text',
+'text' => "กรุณากรอกข้อความให้ถูกต้องคะ พิมพ์ HELP เพื่อดูรายการ" 
+// "text"
+];
+if (strtoupper($text) == "HELP"){	
+$messages = [
+'type' => 'text',
+'text' => "ดูค่าPH พิมพ์ pH "."\n"	];
 
+}
 
-	if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-			
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => "กรุณากรอกข้อความให้ถูกต้องคะ พิมพ์ HELP เพื่อดูรายการ" 
-					// "text"
-			];
-			if (strtoupper($text) == "HELP"){		
-				$messages = [
-				'type' => 'text',
-				'text' => "ดูค่าPH พิมพ์ pH "."\n"		];
-				
-					
-			if (strtoupper($text) == "PH"){		
-				$messages = [
-				'type' => 'text',
-				'text' => "Natural pH : ".$ph ." \n$date\n"];				
-			}
-		
-			// Make a POST Request to Messaging API to reply to sender
-			
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-			echo $result . "\r\n";
-		}
-	}
-	
+if (strtoupper($text) == "PH"){	
+$messages = [
+'type' => 'text',
+'text' => "Natural pH : ".$ph ." \n$date\n"];
+
+}
+
+// Make a POST Request to Messaging API to reply to sender
+$url = 'https://api.line.me/v2/bot/message/reply';
+$data = [
+'replyToken' => $replyToken,
+'messages' => [$messages],
+];
+$post = json_encode($data);
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result . "\r\n";
+}
+}
+
 }
 echo "OK";
-
