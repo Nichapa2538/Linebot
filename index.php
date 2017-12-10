191be0c622e1a0622b8319de4d47aa3cd2e0cabd
Nichapa2538 <nichapa.it@gmail.com>
2017-12-10 16:31:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -42,6 +42,7 @@
                 'text' => "Natural pH : ".$ph ." \n$date\n"];
 
                 }
+                  
 
                 // Make a POST Request to Messaging API to reply to sender
                 $url = 'https://api.line.me/v2/bot/message/reply';
